1b459c815dd2116f2652e46a7d4df72a43d19ba7
rohimexc <rohimexcellent5918@gmail.com>
2025-11-30 17:13:22 +0800

    landing

---
+++ diginus/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include  # ← tambahkan include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('landingpage.urls')),  # ← tambahkan ini
+]
+
+# Tambahkan ini hanya saat development
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
